398a5bde0ddc0dc29611eafa1881ddd59c8eb06a
Stevens Sun <longyc2011@gmail.com>
2018-02-07 23:26:54 +0800

    修改策略

---
+++ src/main/java/com/shl/excel2json/Student.java
@@ -0,0 +1,55 @@
+package com.shl.excel2json;
+
+import java.util.Date;
+
+public class Student {
+    int id;
+    String name;
+    int age;
+    boolean sex;
+    Date birthDay;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public Student(int no, String name, int age, boolean sex, Date birthDay) {
+
+    }
+}
